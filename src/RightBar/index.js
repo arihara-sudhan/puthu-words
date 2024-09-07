67fb52600448b954ec076f9486baca926c404efd
Ariharasudhan <ariharasudhan.s@zohocorp.com>
2024-09-07 13:35:25 +0530

    CHANGES

--- src/RightBar/index.js
+++ src/RightBar/index.js
@@ -5,16 +5,16 @@ import meta from './meta.json';
 
 export default function RightBar() {
     const [letter, setLetter] = useState("");
+    const aToz = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
     
     return (
         <div className={style.parent}>
             {!letter ? (
                 <div className={style.rightbar}>
                     {meta.map((rec, index) => {
-                        const imgSrc = require(`./assets/${rec.imgPath}`);
                         return (
                             <div key={index} onClick={() => setLetter(`${rec.imgPath.split(".")[0]}`)}>
-                                <img src={imgSrc} alt="LETTER"/>
+                                <h1>{aToz[index]}</h1>
                             </div>
                         );
                     })}
